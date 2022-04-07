e6f65c378a445427f5e66e3d6e5aced1b88df169
cloudymous <sayacloud@gmail.com>
2022-04-07 23:20:37 +0800

    Create root routing and handler

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello go-api-git!")
+
+	router := gin.Default()
+	v1 := router.Group("/v1")
+
+	v1.GET("/", rootHandler)
+
+	router.Run(":8080")
+
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"Status": "Response OK",
+	})
 }
