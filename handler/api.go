b5e35ad441a4f3eb2b12a3e38b4165b8b9f8aa6d
cloudymous <sayacloud@gmail.com>
2022-04-08 10:44:25 +0800

    Change name of response message

--- handler/api.go
+++ handler/api.go
@@ -9,7 +9,7 @@ import (
 func StudentGetHandler(c *gin.Context) {
 	id := c.Param("id")
 	c.JSON(http.StatusOK, gin.H{
-		"message": "success",
-		"id":      id,
+		"message":    "success",
+		"student id": id,
 	})
 }
